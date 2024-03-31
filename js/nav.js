5d525a13592b2547d6d439e2920c5f5c2f325586
Jessica Choi <jesymchoi@gmail.com>
2024-03-30 17:13:01 -0700

    added links to nav bar for logged in users

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /******************************************************************************
  * Handling navbar clicks and updating navbar
@@ -7,30 +7,30 @@
 /** Show main list of all stories when click site name */
 
 function navAllStories(evt) {
-  console.debug("navAllStories", evt);
+  console.debug('navAllStories', evt);
   hidePageComponents();
   putStoriesOnPage();
 }
 
-$body.on("click", "#nav-all", navAllStories);
+$body.on('click', '#nav-all', navAllStories);
 
 /** Show login/signup on click on "login" */
 
 function navLoginClick(evt) {
-  console.debug("navLoginClick", evt);
+  console.debug('navLoginClick', evt);
   hidePageComponents();
   $loginForm.show();
   $signupForm.show();
 }
 
-$navLogin.on("click", navLoginClick);
+$navLogin.on('click', navLoginClick);
 
 /** When a user first logins in, update the navbar to reflect that. */
 
 function updateNavOnLogin() {
-  console.debug("updateNavOnLogin");
-  $(".main-nav-links").show();
+  console.debug('updateNavOnLogin');
   $navLogin.hide();
   $navLogOut.show();
+  $loggedIn.show();
   $navUserProfile.text(`${currentUser.username}`).show();
 }
